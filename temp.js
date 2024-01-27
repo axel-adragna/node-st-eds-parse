f609cd465057604d69007ca47f7f29dd401baa8c
Jason Serafin <jtserafin@gmail.com>
2024-01-27 13:32:48 -0500

    delete tests

--- temp.js
+++
@@ -1,5 +0,0 @@
-let EdsParse = require('./index');
-let fs = require('fs');
-let eds = EdsParse.ParseEDS(fs.readFileSync('test-eds/ifm_IOL_Master_AL1322.eds').toString());
-
-console.log(eds, EdsParse.GetAssembly(eds, 199).params);
